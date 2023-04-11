a0383a5dab409c0440774fa03c186ac313849a3e
Shaswat2001 <sis_shaswat@outlook.com>
2023-04-11 18:01:31 +0530

    Added the control system to move the robot

--- scripts/autonomous.py
+++ scripts/autonomous.py
@@ -79,7 +79,7 @@ class MoveForward:
         rospy.loginfo("Stop")
         rospy.sleep(1)
 
-class RotateClockWise:
+class Rotate:
 
     def __init__(self,theta_res):
 
@@ -106,7 +106,8 @@ class RotateClockWise:
         goal_pose.position.y = goal[1]
 
         vel_msg = Twist()
-
+        # goal_angle,current_angle = self.angle_difference(goal_pose)
+        current_difference = self.angle_difference(goal_pose)
         while abs(self.angle_difference(goal_pose)) >= self.theta_res:
 
             vel_msg.linear.x = 0
@@ -116,76 +117,19 @@ class RotateClockWise:
             # Angular velocity in the z-axis.
             vel_msg.angular.x = 0
             vel_msg.angular.y = 0
-            vel_msg.angular.z = -0.2
+            
+            if current_difference > 0:
 
-            # Publishing our vel_msg
-            self.vel_pub.publish(vel_msg)
+                if current_difference > 360 - current_difference:
+                    vel_msg.angular.z = -0.2
+                else:
+                    vel_msg.angular.z = 0.2
 
-            # Publish at the desired rate.
-            self.rate.sleep()
-
-        # Stopping our robot after the movement is over.
-        vel_msg.linear.x = 0
-        vel_msg.angular.z = 0
-        self.vel_pub.publish(vel_msg)
-
-    def angle_difference(self,goal):
-
-        pose = self.current_pose.pose.pose.position
-        current_orientation = self.current_pose.pose.pose.orientation
-
-        rospy.loginfo(f"The position x : {pose.x} and y : {pose.y}")
-        goal_angle = np.rad2deg(math.atan2(goal.position.y - pose.y, goal.position.x - pose.x))%360
-        quaternion = [current_orientation.x,current_orientation.y,current_orientation.z,current_orientation.w]
-        
-        current_angle = np.rad2deg(euler_from_quaternion(quaternion)[-1])%360
-
-        rospy.loginfo(f"The current angle is {current_angle} and goal is {goal_angle}")
-        return goal_angle - current_angle
-
-    def shutdown(self):
-
-        rospy.loginfo("Stop")
-        rospy.sleep(1)
-
-class RotateCounterClockWise:
-
-    def __init__(self,theta_res):
-
-        rospy.on_shutdown(self.shutdown)
-
-        self.vel_pub = rospy.Publisher("/cmd_vel",Twist,queue_size=10)
-        self.pose_sub = rospy.Subscriber("/odom",Odometry,self.pose_callback)
-
-        self.theta_res = theta_res
-        self.current_pose = Odometry()
-        self.rate = rospy.Rate(10)
-
-    def pose_callback(self,pose_data):
-        
-        
-        self.current_pose = pose_data
-        self.current_pose.pose.pose.position.x = round(self.current_pose.pose.pose.position.x,4)
-        self.current_pose.pose.pose.position.y = round(self.current_pose.pose.pose.position.y,4)
-
-    def rotate(self,goal):
-
-        goal_pose = Pose()
-        goal_pose.position.x = goal[0]
-        goal_pose.position.y = goal[1]
-
-        vel_msg = Twist()
-
-        while abs(self.angle_difference(goal_pose)) >= self.theta_res:
-
-            vel_msg.linear.x = 0
-            vel_msg.linear.y = 0
-            vel_msg.linear.z = 0
-
-            # Angular velocity in the z-axis.
-            vel_msg.angular.x = 0
-            vel_msg.angular.y = 0
-            vel_msg.angular.z = 0.2
+            else:
+                if abs(current_difference) > 360 - abs(current_difference):
+                    vel_msg.angular.z = 0.2
+                else:
+                    vel_msg.angular.z = -0.2
 
             # Publishing our vel_msg
             self.vel_pub.publish(vel_msg)
@@ -221,14 +165,26 @@ if __name__=="__main__":
 
     rospy.init_node("autonomous_node")
 
+    rotate_turtlebot = Rotate(1)
+    move_forward_turtlebot = MoveForward(0.1)
+
     while True:
         mp_sub = rospy.Subscriber("/motion_planning_results",motion_planning,results_callback)
         
         if algorithm_path is not None:
             mp_sub = None
             break
+        
+    for i in reversed(range(1,len(algorithm_path))):
 
-    
+        end = [algorithm_path[i].x/100,algorithm_path[i].y/100]
+
+        rotate_turtlebot.rotate(end)
+        move_forward_turtlebot.move(end)
+
+
+
+        
 
 
 
