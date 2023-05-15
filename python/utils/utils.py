ad88b33f7c46e1fd33df1a5d2f293ad2eb4a6fd9
Shaswat2001 <sis_shaswat@outlook.com>
2023-05-15 13:59:40 +0530

    Implemented the 6 dubins path

--- python/utils/utils.py
+++ python/utils/utils.py
@@ -1,14 +1,44 @@
+import numpy as np
+import math
 
-def mod2pi():
+def mod2pi(angle,range_zero_2pi = True):
 
-    pass
+    if range_zero_2pi:
+        return angle%(2*np.pi)
+    else:
+        pass
 
 def theta2RMatrix(angle,radians = True):
 
-    if radians:
+    if not radians:
 
-        pass
+        angle = np.deg2rad(angle)
 
-    else:
+    ctheta = math.cos(angle)
+    stheta = math.sin(angle)
+    Rmat = np.array([[ctheta, -stheta, 0],
+                     [stheta,  ctheta, 0],
+                     [0, 0, 1]])
+    
+    return Rmat
 
-        pass
+def getTmatrix(Rmat,pmat):
+
+    Tmat = np.concatenate((Rmat,pmat),axis=-1)
+    Tmat = np.concatenate((Tmat,np.array([[0,0,0,1]])))
+
+    return Tmat
+
+if __name__ == "__main__":
+
+    angle = 0.523
+
+    R = theta2RMatrix(angle)
+
+    angle = 30
+
+    print(theta2RMatrix(angle,False))
+
+    print(getTmatrix(R,np.array([[0,3,4]]).T))
+
+    
--- scripts/CurveBased/dubins_path.py
+++ scripts/CurveBased/dubins_path.py
@@ -1,50 +1,190 @@
 #!/usr/bin/env python3
 
 import math
+import numpy as np
+from utils.utils import theta2RMatrix,mod2pi,getTmatrix
 
-def mod2pi():
-    pass
+
+# def mod2pi():
+#     pass
 
 class DubinsPath:
 
-    def __init__(self,start,goal,turning_radius):
+    def __init__(self,start,goal,turning_radius,path_type):
 
         self.start = start
         self.goal = goal
         self.turning_radius = turning_radius
+        self.path_type = path_type
 
-    def transform_coordinates(self):
+        self.dubins_list = {"RLR":self.RLR,
+                            "LRL":self.LRL,
+                            "RSR":self.RSR,
+                            "LSL":self.LSL,
+                            "LSR":self.LSR,
+                            "RSL":self.RSL}
 
-        pass
+    def get_angles(self,a,b):
 
+        sinb = math.sin(b)
+        sina = math.sin(a)
+        cosa = math.cos(a)
+        cosb = math.cos(b)
+        cosab = math.cos(a-b)
+
+        return sina,sinb,cosa,cosb,cosab
+    
     def compute_dubins_path(self):
 
-        pass
-    
-    def RSR(self):
+        Tgoal = self.transform_coordinates(self.start,self.goal)
 
-        pass
+        d1,d2,d3,mode = self.compute_dubins_path_from_origin(Tgoal)
+        return d1,d2,d3,mode
 
-    def LSL(self):
+    def transform_coordinates(self,start,goal):
 
-        pass
+        Rmatrix = theta2RMatrix(start[-1])
+        Tmat =  getTmatrix(Rmatrix,np.array([[start[0],start[1],1]]).T)
+        print(f"{np.linalg.inv(Tmat).shape}")
+        transformed_goal = np.linalg.inv(Tmat)@np.array([[goal[0],goal[1],0,1]]).T
 
-    def LSR(self):
+        Tgoal = [transformed_goal[0][0],transformed_goal[1][0],goal[-1]-start[-1]]
+        print(f"Transformed goal : {Tgoal}")
 
-        pass
+        return Tgoal
 
-    def RSL(self):
+    def compute_dubins_path_from_origin(self,goal):
 
-        pass
+        theta = math.atan2(goal[1],goal[0])
+        D = np.hypot(goal[0],goal[1])
 
-    def LRL(self):
+        alpha = mod2pi(-theta)
+        beta = mod2pi(goal[-1] - theta)
 
-        pass
+        if self.path_type:
+            d1,d2,d3,mode = self.dubins_list[self.path_type](alpha,beta,D,self.turning_radius)
+            return d1,d2,d3,mode
+        
+        best_mode = None
+        best_cost = math.inf
 
-    def RLR(self):
+        for _,path_func in self.dubins_list.items():
 
-        pass
+            d1,d2,d3,mode = path_func(alpha,beta,D,self.turning_radius)
+            print(f"mode : {mode}, d1 = {d1}, d2 = {d2}, d3 = {d3}")
+            if d1 == None:
+                continue
+
+            cost = d1 + d2 +d3
+
+            if cost < best_cost:
+                best_cost = cost
+                best_mode,best_d1,best_d2,best_d3 = mode,d1,d2,d3
+
+        
+        return best_d1,best_d2,best_d3,best_mode
+
+    def RSR(self,alpha,beta,D,rad):
+
+        sina,sinb,cosa,cosb,cosab = self.get_angles(alpha,beta)
+        mode = ["R","S","R"]
+        d_squared = D**2 + 2*(rad**2) - 2*cosab*(rad**2) + 2*rad*D*(sinb - sina)
+        temp = math.atan2(rad*(cosa - cosb),D + rad*(-sina + sinb))
+
+        if d_squared < 0:
+            return None,None,None,mode
+
+        d1 = mod2pi(alpha - temp)
+        d2 = math.sqrt(d_squared)
+        d3 = mod2pi(-beta + temp)
+
+        return d1,d2,d3,mode
+
+    def LSL(self,alpha,beta,D,rad):
+
+        sina,sinb,cosa,cosb,cosab = self.get_angles(alpha,beta)
+        mode = ["L","S","L"]
+        d_squared = D**2 + 2*(rad**2) - 2*cosab*(rad**2) + 2*rad*D*(sina - sinb)
+        temp = math.atan2(rad*(cosb - cosa),D + rad*(sina - sinb))
+
+        if d_squared < 0:
+            return None,None,None,mode
+
+        d1 = mod2pi(-alpha + temp)
+        d2 = math.sqrt(d_squared)
+        d3 = mod2pi(beta - temp)
+
+        return d1,d2,d3,mode
+
+    def LSR(self,alpha,beta,D,rad):
+
+        sina,sinb,cosa,cosb,cosab = self.get_angles(alpha,beta)
+        mode = ["L","S","R"]
+        d_squared = D**2 - 2*(rad**2) + 2*cosab*(rad**2) + 2*rad*D*(sina + sinb)
+        
+        if d_squared < 0:
+            return None,None,None,mode
+        
+        d2 = math.sqrt(d_squared)
+
+        temp = math.atan2(-rad*(cosb + cosa),D + rad*(sina + sinb)) - math.atan2(-2,d2)
+        d1 = mod2pi(-alpha + temp)
+        d3 = mod2pi(-mod2pi(beta) + temp)
+
+        return d1,d2,d3,mode
+
+    def RSL(self,alpha,beta,D,rad):
+
+        sina,sinb,cosa,cosb,cosab = self.get_angles(alpha,beta)
+        mode = ["R","S","L"]
+        d_squared = D**2 - 2*(rad**2) + 2*cosab*(rad**2) - 2*rad*D*(sina + sinb)
+        
+        if d_squared < 0:
+            return None,None,None,mode
+        
+        d2 = math.sqrt(d_squared)
+        
+        temp = math.atan2(rad*(cosb + cosa),D - rad*(sina + sinb)) - math.atan2(2,d2)
+        d1 = mod2pi(alpha - temp)
+        d3 = mod2pi(beta - temp)
+
+        return d1,d2,d3,mode
+
+    def RLR(self,alpha,beta,D,rad):
+
+        sina,sinb,cosa,cosb,cosab = self.get_angles(alpha,beta)
+        mode = ["R","L","R"]
+        temp = (6*(rad**2) - D**2 + 2*cosab*(rad**2) + 2*rad*D*(sina - sinb))/8
+        
+        if abs(temp) > 1:
+            return None,None,None,mode
+        
+        d2 = mod2pi(2*math.pi - math.acos(temp))        
+        d1 = mod2pi(alpha - math.atan2(rad*(cosa - cosb), D - rad*(-sina + sinb)) + d2 / 2.0)
+        d3 = mod2pi(alpha - beta - d1 + d2)
+
+        return d1,d2,d3,mode
+
+    def LRL(self,alpha,beta,D,rad):
+
+        sina,sinb,cosa,cosb,cosab = self.get_angles(alpha,beta)
+        mode = ["L","R","L"]
+        temp = (6*(rad**2) - D**2 + 2*cosab*(rad**2) + 2*rad*D*(sinb - sina))/8
+        
+        if abs(temp) > 1:
+            return None,None,None,mode
+        
+        d2 = mod2pi(2*math.pi - math.acos(temp))        
+        d1 = mod2pi(-alpha - math.atan2(rad*(cosa - cosb), D + rad*(sina - sinb)) + d2 / 2.0)
+        d3 = mod2pi(-alpha + mod2pi(beta) - d1 + mod2pi(d2))
+
+        return d1,d2,d3,mode
 
 if __name__ == "__main__":
 
-    pass
+    dubinsPath = DubinsPath([1,1,np.deg2rad(45)],[0,0,np.deg2rad(-45)],1,None)
+    d1,d2,d3,mode = dubinsPath.compute_dubins_path()
+
+    print(f"mode : {mode}, d1 = {d1}, d2 = {d2}, d3 = {d3}")
+
+
